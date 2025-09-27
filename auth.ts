600a951cea2d2e83b3c5ddd42e8785cc73633940
Kevin Spinks <108372434+k-spinks@users.noreply.github.com>
2025-09-27 16:21:46 -0500

    refactor: streamline authentication by removing unused GitHub provider and consolidating Google provider setup

--- auth.ts
+++ auth.ts
@@ -1,10 +1,15 @@
 import NextAuth from "next-auth";
-import Github from "next-auth/providers/github";
-import Google from "next-auth/providers/google";
+import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "./lib/prisma";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  providers: [Github, Google],
+  providers: [
+    GoogleProvider({
+      clientId: process.env.AUTH_GOOGLE_ID!,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+    }),
+  ],
   adapter: PrismaAdapter(prisma),
+  secret: process.env.NEXTAUTH_SECRET, // important for production
 });
--- lib/auth-actions.ts
+++ lib/auth-actions.ts
@@ -2,10 +2,6 @@
 
 import { signIn, signOut } from "@/auth";
 
-// export const login = async (provider: string) => {
-//   await signIn(`${provider}`, { redirectTo: "/trips" });
-// };
-
 export const login = async () => {
   await signIn(`google`, { redirectTo: "/trips" });
 };
